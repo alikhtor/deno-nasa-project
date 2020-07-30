9e2083a6208d9bb45e79802e32d0df61b1e437bb
alikhtorovych <alikhtorovych@integrity.com.ua>
2020-07-30 09:35:23 +0300

    middleware

--- mod.ts
+++ mod.ts
@@ -3,6 +3,12 @@ import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+app.use(async (ctx, next) => {
+    
+    await next();
+    console.log(`${ctx.request.method} ${ctx.request.url}`)
+});
+
 app.use((ctx) => {
     ctx.response.body = `                                             _______ _______ _______
                                             |   |   |    ___|    |  |
